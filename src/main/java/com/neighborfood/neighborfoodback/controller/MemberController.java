28b229e5c7e0133c32b5cbc83f550c7a00ccc65b
jhin <com.jinnn99@gmail.com>
2023-05-09 14:15:29 +0900

    회원정보수정(임시), 음식점리스트얻기(임시)

--- src/main/java/com/neighborfood/neighborfoodback/controller/MemberController.java
+++ src/main/java/com/neighborfood/neighborfoodback/controller/MemberController.java
@@ -12,8 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-
 @CrossOrigin(origins = "*")
 @Slf4j
 @RestController
