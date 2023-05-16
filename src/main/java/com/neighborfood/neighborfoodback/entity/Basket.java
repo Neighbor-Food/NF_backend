bf58be5b44ce69ed9a9e4a7770b1b7717da8cf28
DrHagha <coby4313@gmail.com>
2023-05-16 16:53:20 +0900

    borad entity 작성

---
+++ src/main/java/com/neighborfood/neighborfoodback/entity/Basket.java
@@ -0,0 +1,41 @@
+package com.neighborfood.neighborfoodback.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Basket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer basket_no;
+    private Integer menu_quantity;
+
+    @JsonIgnore
+    @JoinColumn(name = "board_no")
+    private Board board;
+
+    @JsonIgnore
+    @JoinColumn(name = "member_no")
+    private Member writer;
+
+    @JsonIgnore
+    @JoinColumn(name = "menu_no")
+    private Menu menu;
+
+}
