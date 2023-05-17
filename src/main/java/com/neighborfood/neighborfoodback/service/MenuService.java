bf12be7450f92becc80a98c287e8db749824bc4c
DrHagha <coby4313@gmail.com>
2023-05-17 15:08:33 +0900

    menuService getList 작성

--- src/main/java/com/neighborfood/neighborfoodback/service/MenuService.java
+++ src/main/java/com/neighborfood/neighborfoodback/service/MenuService.java
@@ -2,12 +2,31 @@ package com.neighborfood.neighborfoodback.service;
 
 import java.util.List;
 
-import com.neighborfood.neighborfoodback.entity.Menu;
+import javax.management.RuntimeErrorException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.neighborfood.neighborfoodback.entity.Menu;
+import com.neighborfood.neighborfoodback.repository.MenuRepostory;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
 public class MenuService {
+    @Autowired
+    private MenuRepostory menuRepostory;
 
     public List<Menu> getList() {
-        return null;
+        List<Menu> menuList = menuRepostory.findAllByRestaurantNo();
+        if(menuList.isEmpty()){
+            log.warn("menu list is empty");
+            throw new RuntimeException("menu list is empty");
+        }
+        else{
+            return menuList;
+        }
     }
     
 }
